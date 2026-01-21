aaf28c2acc859f982fa41b8defcf3453bae1fb6d
a06039487-star <a06039487@gmail.com>
2026-01-22 00:36:47 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,13 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("trip-planner").then(cache => {
+      return cache.addAll(["/", "/index.html"]);
+    })
+  );
+});
 
-const cacheName='osaka-travel-cache-v1';
-const filesToCache=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(cacheName).then(cache=>cache.addAll(filesToCache)));});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(response=>response||fetch(e.request)));});
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
